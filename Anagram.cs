36f44f84a36398bc5d8c36a109b9ba4ba5180c13
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-01-02 11:11:43 +0530

    Algorithm Programs

---
+++ Anagram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class Anagram
+    {
+        public void AnagramDisplay()
+        {
+            string str1 = "heater";
+            string str2 = "reheat";
+
+            char[] char1 = str1.ToLower().ToCharArray();
+            char[] char2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(char1);
+            Array.Sort(char2);
+
+            string val1 = new string(char1);
+            string val2 = new string(char2);
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Anagram");
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.DisplayInserctionSort();
+
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.DisplayBubbleSort();
+
+            Anagram anagram = new Anagram();
+            anagram.AnagramDisplay();
+
+            PrimeNumber primeNumber = new PrimeNumber();
+            primeNumber.FindPrimeNumber(1000);
+
+        }
+    }
+}
